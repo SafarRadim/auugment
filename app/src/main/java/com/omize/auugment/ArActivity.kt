10ef67730cb8812b5b224996a8a9589d972e2871
MartinWeiss01 <38295323+MartinWeiss01@users.noreply.github.com>
2020-05-11 12:01:32 +0200

    Update ArActivity.kt

--- app/src/main/java/com/omize/auugment/ArActivity.kt
+++ app/src/main/java/com/omize/auugment/ArActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.widget.ImageButton
 
 class ArActivity : AppCompatActivity() {
@@ -11,7 +12,7 @@ class ArActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ar)
-
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
         val bckbtn = findViewById<ImageButton>(R.id.goBackButton)
         bckbtn.setOnClickListener {
             finish()
