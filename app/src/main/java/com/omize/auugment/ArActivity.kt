e64483400f213b3b0204edd1eacaa6009a3ce8f3
MartinWeiss01 <38295323+MartinWeiss01@users.noreply.github.com>
2020-05-11 02:35:58 +0200

    Added back button to AR session

--- app/src/main/java/com/omize/auugment/ArActivity.kt
+++ app/src/main/java/com/omize/auugment/ArActivity.kt
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.content.Intent
+import android.widget.ImageButton
 
 class ArActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ar)
+
+        val bckbtn = findViewById<ImageButton>(R.id.goBackButton)
+        bckbtn.setOnClickListener {
+            finish()
+        }
 
     }
 }
